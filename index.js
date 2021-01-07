d4dd5da43e8c94eb2eea7632dada9844f6bc02d2
Shoubhik Banerjee <shoubhik4hexa@gmail.com>
2021-01-07 21:13:45 +0530

    http-delayed-response package added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
 //const app = dialogflow({debug:true});
 const app = conversation();
+var DelayedResponse = require('http-delayed-response');
 
 
 app.handle('sayHello', conv => {
