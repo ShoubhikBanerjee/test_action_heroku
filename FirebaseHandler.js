7e9eadc9f2bed33216d3e415600bfec44c5e838a
Shoubhik Banerjee <shoubhik4hexa@gmail.com>
2021-01-12 15:24:16 +0530

    Update FirebaseHandler.js

--- FirebaseHandler.js
+++ FirebaseHandler.js
@@ -1,13 +1,13 @@
 
 var admin = require("firebase-admin");
 var serviceAccount = require("./Keys/auto-bot-3c17c-firebase-adminsdk-rsdhn-19b8c738ab.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
+const db = admin.firestore();
 module.exports.updateFirebaseDocument = function (document_name, key, value) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
-    
-    const db = admin.firestore();
+   
     var key2 = ("app_trigger_config." + key).toString();
     console.log("document_name => ", document_name)
     var docRef = db.collection('autobot-device-handler').doc(document_name)
